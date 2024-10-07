3577540c568beb1da3d6146526d2e64e4bd3e94a
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-07 17:37:00 +0100

    Initial App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import LandingPage from './components/LandingPage/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
