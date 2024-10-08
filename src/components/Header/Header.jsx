b836ac9fceb4ab3f8db8c9e7ac7c21d44ca2cf51
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-08 20:40:34 +0100

    build(header): build the header component and styled it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
+import logo from './assets/logo.svg'
+// import menubar from './assets/menu-bar.svg'
+import './Header.css'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='header'>
+      <img src={logo} className="logo" alt="" />
+      <ul className="navbar">
+        <li><a href=""></a>Pricing</li>
+        <li><a href=""></a>Client Galleries</li>
+        <li><a href=""></a>Login</li>
+        <button>Try 1 month free</button>
+      </ul>
+    </section>
   )
 }
 
