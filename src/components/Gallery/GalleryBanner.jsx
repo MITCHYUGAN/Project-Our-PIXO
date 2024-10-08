692d53d345eb44686bf41fefbe73bf761f19044a
CodingGimmic <codinggimmic@gmail.com>
2024-10-08 18:10:53 +0100

    added: showcase sections, dropdown selection functionality, showcase section styling

---
+++ src/components/Gallery/GalleryBanner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function GalleryBanner() {
+  return (
+    <div className='gallery-banner'>
+      <div className='banner-image-wrapper'>
+        <img src="./banner-image.jpeg" alt="" />
+      </div>
+        <div className='banner-text'>
+      <h1>
+        Hop into our World of <br /><span>Creativity</span>
+      </h1>
+      <p>
+        OurPixo is used by creatives to create beautiful galleries and cover
+        photos and deliver them in style.
+      </p>
+        </div>
+    </div>
+  );
+}
+
+export default GalleryBanner
