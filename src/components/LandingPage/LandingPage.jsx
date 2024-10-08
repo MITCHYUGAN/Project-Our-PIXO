a15dd8f4d5a60c874e9946413788886f50ea109a
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-08 20:41:30 +0100

    build(landingPage): build the landingPage component and styled it

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,19 @@
+import landingImg from './assets/landing-img.svg'
+import './LandingPage.css'
+
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <section className="landingPage">
+      <div className='landing-contents'>
+        <h4>STAND OUT AS A CREATIVE</h4>
+        <h1>The best way to deliver your work to clients in a professional manner</h1>
+        <p>OurPixo gives you tools at the tip of your fingers to create, customize, and deliver your work to clients wherever they are.</p>
+        <button>Try 1 month free</button>
+      </div>
+      <div className='landing-img'>
+        <img src={landingImg} alt="" />
+      </div>
+    </section>
   )
 }
 
