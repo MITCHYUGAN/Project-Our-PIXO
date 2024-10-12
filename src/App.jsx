5d4703f7746d8a2f7ec8d33caa0277c648edf0cc
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-13 00:22:42 +0100

    refactor: refactor file to be more clean and easy to read

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,19 @@ import MainComponent from './components/Gallery/MainComponent'
 import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import WhyOurPIXO from './components/WhyOurPIXO/WhyOurPIXO'
-import GetStarted from './components/GetStarted/GetStarted'
-import GetPaidInCurrencies from './components/GetPaidInCurrencies/GetPaidInCurrencies'
-import EasyTools from './components/EasyTools/EasyTools'
-import Testimonials from './components/Testimonials/Testimonials'
-import GetStartedForFree from './components/GetStartedForFree/GetStartedForFree'
 import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="showcase" element={<MainComponent />} />
-      </Routes>
-    </Router>
-      <WhyOurPIXO />
-      <GetStarted/>
-      <GetPaidInCurrencies/>
-      <EasyTools />
-      <Testimonials/>
-      <GetStartedForFree/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="showcase" element={<MainComponent />} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   );
