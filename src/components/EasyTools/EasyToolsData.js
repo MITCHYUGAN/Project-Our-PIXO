6050bba728cf91859901b5470fb4cafa4514cd63
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-12 21:13:56 +0100

    build(EasyTools): build EasyTools component, and styled it, including responsiveness

---
+++ src/components/EasyTools/EasyToolsData.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+export const EasyToolsData = [
+    {
+        img: require("./assets/img1.svg"),
+        h3: "Customizable Photo/Video Galleries",
+        p: "Showcase your work to the client in beautiful personalized galleries."
+    }, 
+    {
+        img: require("./assets/img2.svg"),
+        h3: "Collect Online/Offline Payments",
+        p: "Avoid chasing clients for your balance after the job! Ensure the client pays your balance before downloading the files."
+    }, 
+    {
+        img: require("./assets/img3.svg"),
+        h3: "Instant Notification",
+        p: "Stay updated with real-time email and WhatsApp alerts."
+    }, 
+]
