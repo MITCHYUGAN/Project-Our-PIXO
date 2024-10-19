bddb58216c8229829487f88a40eaf773a0a7bb6a
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-19 17:45:32 +0100

    feat: Add the features page to a route so its displays on the website in it's specific route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer'
+import Features from './Pages/Features/Features';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="showcase" element={<MainComponent />} />
+          <Route path='features' element={<Features/>} />
         </Routes>
       </Router>
       <Footer />
