4f0feb03c44d2f08bfa9a8ae6fe582fd9d3de590
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-24 22:55:09 +0100

    feat(HowItWorks): added the new HowItWorks Page to reflect on it's respective url

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer'
 import Features from './Pages/Features/Features';
+import HowItWorks from './Pages/HowItWorks/HowItWorks';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="showcase" element={<MainComponent />} />
           <Route path="features" element={<Features/>} />
+          <Route path="howitworks" element={<HowItWorks/>} />
         </Routes>
       </Router>
       <Footer />
