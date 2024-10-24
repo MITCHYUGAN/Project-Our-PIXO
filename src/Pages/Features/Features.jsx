c1333a914f59e04946960bdd6c7642591ffc0310
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-24 07:14:14 +0100

    build(Features): build the Features Page, add the images and styled it, including responsiveness

--- src/Pages/Features/Features.jsx
+++ src/Pages/Features/Features.jsx
@@ -1,34 +1,41 @@
-import features from "./FeaturesData"
+import { FeaturesData } from "./FeaturesData"
+import featureBanner1 from "./assets/featureBanner1.svg"
+import featureBanner2 from "./assets/featureBanner2.svg"
+import featureBanner3 from "./assets/featureBanner3.svg"
 
 import "./Features.css"
+import GetStartedForFree from "../../components/GetStartedForFree/GetStartedForFree"
+import EasyTools from "../../components/EasyTools/EasyTools"
 
 const Features = () => {
   return (
-    <section className="features">
-        <div className="featuresHero">
+    <main className="features">
+        <header className="featuresHero">
             <h1>The Perfect Tools For You</h1>
-        </div>
-        <div>
-            <h1>Easy to use tools</h1>
-            <div>
-                {
-                    features.map(({key, img, h3, p}) => {
-                        return (
-                            <div className="tool" key={key}>
-                                <div>
-                                    <img src={img} alt="" />
-                                </div>
-                                <div>
-                                    <h3>{h3}</h3>
-                                    <p>{p}</p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+        </header>
+        <EasyTools easytoolsdata={FeaturesData} />
+        <section className="giveBusinessProfTouch">
+            <h1 className="giveBusinessProfTouchH1">Give your business a professional touch</h1>
+            <div className="giveBusinessProfTouchContents">
+                <div className="profTouch">
+                    <h3>Gallery Creation and Customization</h3>
+                    <p>Create unique and beautiful cover photos and galleries for your client. You are given full customization rights to be as creative as you wish.</p>
+                    <img src={featureBanner1} alt="Gallery Creation and Customization Image" />
+                </div>
+                <div className="profTouch">
+                    <h3>Creation of Sets</h3>
+                    <p>Gather all the memories of weddings, events and parties into one file, with different folders. Create a set and arrange all parts of the event accordingly, making it easier to access files.</p>
+                    <img src={featureBanner2} alt="Creation of Sets Image" />
+                </div>
+                <div className="profTouch">
+                    <h3>Business Tracking</h3>
+                    <p>Track projects, manage bookings, see payments, and get updates on how your business is doing. It makes your work easier, saves time, and helps you stay organized.</p>
+                    <img src={featureBanner3} alt="Business Tracking Image" />
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <GetStartedForFree />
+    </main>
   )
 }
 
--- src/Pages/Features/FeaturesData.js
+++ src/Pages/Features/FeaturesData.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const features = [
+export const FeaturesData = [
     {
         img: require("./assets/img1.svg"),
         h3: "Customizable Photo/Video Galleries",
@@ -15,6 +15,29 @@ const features = [
         h3: "Instant Notification",
         p: "Stay updated with real-time email and WhatsApp alerts."
     }, 
+    {
+        img: require("./assets/img4.svg"),
+        h3: "Client Selection",
+        p: "Seamlessly send pictures and videos to your client for selection and feedback before you edit, frame, print, or add to photobook."
+    }, 
+    {
+        img: require("./assets/img5.svg"),
+        h3: "Framing, Printing and Photo-Book Requests",
+        p: "Upsell your picture framing, printing and photobook services to your clients."
+    }, 
+    {
+        img: require("./assets/img6.svg"),
+        h3: "Business Analytics Dashboard",
+        p: "Harness business insights for strategic decisions and growth."
+    }, 
+    {
+        img: require("./assets/img7.svg"),
+        h3: "Online Portfolio & Booking",
+        p: "Elevate your online presence and with a beautiful portfolio and booking page."
+    }, 
+    {
+        img: require("./assets/img8.svg"),
+        h3: "Invoicing &Calendar Management",
+        p: "Never miss a client shoot again! Simplify billing and jobs scheduling for peak efficiency."
+    }, 
 ]
-
-export default features
