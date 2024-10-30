553e58227da09a92e3b6600e9b9a097ae66e6c72
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-30 16:08:10 +0100

    Modified GetPaidInCurrencies styling to fix design

--- src/components/GetPaidInCurrencies/GetPaidInCurrencies.jsx
+++ src/components/GetPaidInCurrencies/GetPaidInCurrencies.jsx
@@ -11,7 +11,7 @@ const GetPaidInCurrencies = () => {
       <img src={circle} className="circle2" alt="" />
       <h1>Get paid in different currencies!</h1>
       <p>Accept payments globally with support for multiple currencies added to the current local methods, making it easy for your international customers to pay.</p>
-      <button>COMING SOON</button>
+      <span>COMING SOON</span>
       <img src={circle} className="circle3" alt="" />
       <img src={triangle} className="triangle2" alt="" />
       <img src={circle} className="circle4" alt="" />
