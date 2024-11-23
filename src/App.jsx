eb6a1c475554a2ba86929fe1b239379a46a6ddcf
Jahkamso <uzoekwejahkamso@gmail.com>
2024-11-23 16:08:34 +0100

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Features from './Pages/Features/Features';
 import FAQ from './Pages/Faq/Faq';
 import HowItWorks from './Pages/HowItWorks/HowItWorks';
 import ScrollToTop from './ScrollToTop';
-import PrivacyPolicy from './Pages/PrivacyPolicy/PrivacyPolicy';
 
 
 function App() {
