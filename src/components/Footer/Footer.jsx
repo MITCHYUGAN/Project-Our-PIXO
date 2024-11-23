0e849c6e518e6c2f283277d9145a9e0ef30b3013
Jahkamso <uzoekwejahkamso@gmail.com>
2024-11-23 16:01:26 +0100

    added privacy policy page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -92,7 +92,7 @@ const Footer = () => {
               <h3>Company</h3>
               <ul>
                 <li>
-                  <Link to="https://ourpixo.com/policy/" target="_blank">
+                  <Link to="/privacy-policy/" target="_blank">
                     Privacy Policy
                   </Link>
                 </li>
