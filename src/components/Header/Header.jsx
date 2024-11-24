8708eaae639957491f603047c96654d72ddc56ad
Jahkamso <uzoekwejahkamso@gmail.com>
2024-11-24 01:05:32 +0100

    fixed: navigation spacing and added functionality to close navbar when user clicks outside the navigation box

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,57 @@
-import { Link } from 'react-router-dom'
-import logo from './assets/logo.svg'
-import menubar from './assets/menu-bar.svg'
-import menubarcancel from './assets/menubar-cancel.svg'
-import './Header.css'
-import { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import logo from "./assets/logo.svg";
+import menubar from "./assets/menu-bar.svg";
+import menubarcancel from "./assets/menubar-cancel.svg";
+import "./Header.css";
+import { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [activeNav, setActiveNav] = useState("");
+  const [menuBar, setMenuBar] = useState(menubar);
+  const [navButtonAriaText, setNavButtonAriaText] = useState("Nav List Open");
 
-  const [activeNav, setActiveNav] = useState("")
-  const [menuBar, setMenuBar] = useState(menubar)
-  const [navButtonAriaText, setNavButtonAriaText] = useState("Nav List Open")
-
-  const locaton = useLocation()
+  const location = useLocation();
+  const navRef = useRef(null); // Ref for the navigation menu
 
   // Dynamic header background
   const getBackgroundColor = () => {
-    if(location.pathname === "/") {
-      return "#fff"
-    } else if(location.pathname === "/howitworks") {
+    if (location.pathname === "/") {
+      return "#fff";
+    } else if (location.pathname === "/howitworks") {
       return "#B4ECCD";
-    } else if(location.pathname === "/features") {
+    } else if (location.pathname === "/features") {
       return "#FFECBA";
     }
-  }
+  };
 
   function openNav() {
     if (activeNav === "") {
-      setActiveNav("active")
-      setMenuBar(menubarcancel)
-      setNavButtonAriaText("Nav List Close")
+      setActiveNav("active");
+      setMenuBar(menubarcancel);
+      setNavButtonAriaText("Nav List Close");
     } else {
-      setActiveNav("")
-      setMenuBar(menubar)
-      setNavButtonAriaText("Nav List Open")
+      setActiveNav("");
+      setMenuBar(menubar);
+      setNavButtonAriaText("Nav List Open");
     }
   }
+
+  // Close the navigation menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setActiveNav("");
+        setMenuBar(menubar);
+        setNavButtonAriaText("Nav List Open");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav
@@ -44,7 +60,7 @@ const Header = () => {
       className="header"
       style={{ backgroundColor: getBackgroundColor() }}
     >
-      <div className="headerWrapper">
+      <div className="headerWrapper" ref={navRef}>
         <Link to="/">
           <img src={logo} className="logo" alt="" />
         </Link>
@@ -57,7 +73,11 @@ const Header = () => {
           <img src={menuBar} alt="" />
         </button>
         <ul className={`navbar ${activeNav}`}>
-        <li><Link to="https://pricing.pixomaticc.com/" target="_blank">Pricing</Link></li>
+          <li>
+            <Link to="https://pricing.pixomaticc.com/" target="_blank">
+              Pricing
+            </Link>
+          </li>
           <li>
             <Link to="/showcase" onClick={openNav}>
               Client Galleries
@@ -81,6 +101,6 @@ const Header = () => {
       </div>
     </nav>
   );
-}
+};
 
-export default Header
+export default Header;
