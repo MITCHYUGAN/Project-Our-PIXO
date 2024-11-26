59bbedbd55ad334742c634991b4f3a2bd60902c4
Jahkamso <uzoekwejahkamso@gmail.com>
2024-11-26 09:14:38 +0100

    made a change in the markteplace section

---
+++ src/Pages/Gallery/ComingSoon1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ComingSoon1() {
+  return (
+    <div className="coming-soon-section">
+      <h2>Coming Soon</h2>
+      <p>
+      Connect with new clients and opportunities. Showcase your services, 
+      reach a wider audience, and boost your revenue. Network and collaborate 
+      with other professionals, helping you grow your business even further.
+      </p>
+    </div>
+  );
+}
+
+export default ComingSoon1
